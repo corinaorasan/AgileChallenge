81392c7d94d716a50c456630ea9e8c3c14f231ec
Orasan Corina <62331686+corinaorasan171015@users.noreply.github.com>
2020-12-11 12:32:49 +0200

    Added a class for errors and exception messages.
    Added Messages class.
    Small code refactoring.

---
+++ AgileCoffeeShop/Messages.cs
@@ -0,0 +1,30 @@
+﻿namespace AgileCoffeeShop
+{
+    public static class Messages
+    { 
+        public static string InvalidURLMessage
+        {
+            get
+            {
+                return "Invalid URL. Program will exit.\n Press any key to continue...";
+            }
+        }
+        public static string InvalidCoordinateMessage(string coordinateName)
+        {
+            var message = $"Invalid value for {coordinateName} coordinate. Program will exit.\nPress any key to continue...";
+            return message;
+        }
+
+        public static string InvalidCoordinatesException(string exceptionMessage)
+        {
+            var message = $"Invalid coordinates. {exceptionMessage} Program will exit.\nPress any key to continue...";
+            return message;
+        }
+        public static string InvalidURLException(string exceptionMessage)
+        {
+            var message = $"Invalid URL. {exceptionMessage} Program will exit.\nPress any key to continue...";
+            return message;
+        }
+
+    }
+}
